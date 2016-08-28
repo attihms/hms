4bc4c1afc3b5f0f4a8a05156e205348d6d68824f
The Anh <atti.hms@gmail.com>
2016-08-28 08:52:14 +0700

    Database: initialize data for referenced tabbes

--- migrations/20160825144512-create-tables.js
+++ migrations/20160825144512-create-tables.js
@@ -40,12 +40,19 @@ module.exports = {
     },
 
     down: function (queryInterface, Sequelize) {
-        return queryInterface.dropTable('booking_agent');
-        return queryInterface.dropTable('payment_method');
-        return queryInterface.dropTable('room_type');
-        return queryInterface.dropTable('reservations');
-        return queryInterface.dropTable('registration');
-        return queryInterface.dropTable('bill_detail');
-        return queryInterface.dropTable('bill');
+        return queryInterface.dropTable('booking_agent')
+        .then(function() {
+            return queryInterface.dropTable('payment_method');
+        }).then(function() {
+            return queryInterface.dropTable('room_type');
+        }).then(function() {
+            return queryInterface.dropTable('reservation');
+        }).then(function() {
+            return queryInterface.dropTable('registration');
+        }).then(function() {
+            return queryInterface.dropTable('bill_detail');
+        }).then(function() {
+            return queryInterface.dropTable('bill');
+        });
     }
 };
