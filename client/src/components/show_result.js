afe39595c8131fa7bd1f7908063bbf6e18e6dbf6
The Anh <atti.hms@gmail.com>
2016-10-12 00:27:04 +0700

    Move client to a submodule.

--- client/src/components/show_result.js
+++
@@ -1,8 +0,0 @@
-let showResults = (value) => new Promise((resolve, reject) => {  
-   setTimeout(() => {
-   	window.alert(`You have submited: \n\n${Json.stringify(value, null, 2)}`);
-   	resolve();
-   }, 1000);
-});
-
-export default showResults
